54eaa35fe59d6d6a41f6e21c3bf85bbfee3074ea
Surbhi jain <Surbhi.zain10@gmail.com>
2025-07-21 14:15:43 +0530

    Smart Agriculture

---
+++ Backend/controllers/testController.js
@@ -0,0 +1,4 @@
+
+exports.hello = (req, res) => {
+    res.status(200).json({ message: "Hurray! Your CropQ APIs are working" });
+};
---
+++ Backend/routes/contactRouter.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const contactController = require('../controllers/contactController');
+
+router.post('/create', contactController.createContact);
+router.get('/getAllContacts/:subject?', contactController.getAllContacts);
+
+module.exports = router;
